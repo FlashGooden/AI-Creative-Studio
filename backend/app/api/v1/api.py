7502170b076c588b8573de6bd6b78918fbec21e2
FlashGooden <Tro12185@gmail.com>
2025-07-07 18:36:27 -0400

    feat: Testing route set up

--- backend/app/api/v1/api.py
+++ backend/app/api/v1/api.py
@@ -1,6 +1,10 @@
 from fastapi import APIRouter
+from app.api.v1.endpoints import test
 
 api_router = APIRouter()
+
+# Include test endpoints
+api_router.include_router(test.router, prefix="/test", tags=["test"])
 
 
 @api_router.get("/status")
