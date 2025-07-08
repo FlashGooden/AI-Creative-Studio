b1993029172310d33419672e20237bcbd779ca52
FlashGooden <Tro12185@gmail.com>
2025-07-08 01:41:25 -0400

    feat: Add auth and user routes

--- backend/app/api/v1/api.py
+++ backend/app/api/v1/api.py
@@ -1,10 +1,12 @@
 from fastapi import APIRouter
-from app.api.v1.endpoints import test
+from app.api.v1.endpoints import test, auth, users
 
 api_router = APIRouter()
 
-# Include test endpoints
+# Include endpoints
 api_router.include_router(test.router, prefix="/test", tags=["test"])
+api_router.include_router(auth.router, prefix="/auth", tags=["authentication"])
+api_router.include_router(users.router, prefix="/users", tags=["users"])
 
 
 @api_router.get("/status")
